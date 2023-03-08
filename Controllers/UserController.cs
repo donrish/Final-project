665a6bd2e2d457f881bf341a2d64e4975fd7fd19
Rishab S Mardia <113574308+donrish@users.noreply.github.com>
2023-03-08 13:53:52 +0530

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using DelsProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DelsProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Register(User u)
+        {
+            if (u.UserType == "Customer")
+            {
+                return this.RedirectToAction("Create", "Customer");
+            }
+            else if (u.UserType == "Executive")
+            {
+                return this.RedirectToAction("Create", "Executive");
+            }
+            return View();
+        }
+        public ActionResult Login(User u)
+        {
+            if (u.UserType == "Customer")
+            {
+                return this.RedirectToAction("Login", "Customer");
+            }
+            else if (u.UserType == "Executive")
+            {
+                return this.RedirectToAction("Login", "Executive");
+            }
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+       
+
+
+
+    }
+}
