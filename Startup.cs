20efcb01eb32e064be446cef831266203df32a4b
Rishab S Mardia <113574308+donrish@users.noreply.github.com>
2023-03-08 14:27:37 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+using DelsProject.Models;
+using DelsProject.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DelsProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DeliveryContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultCon")));
+            services.AddScoped<IRepo<Customer>, CustomerManager>();
+            services.AddScoped<IRepo<Executive>, ExecutiveManager>();
+            services.AddScoped<IRepo<Booking>, BookingManager>();
+            services.AddControllersWithViews();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
