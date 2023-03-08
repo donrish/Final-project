4cb142f72cf5c36672010fb1081905609b72c371
Rishab S Mardia <113574308+donrish@users.noreply.github.com>
2023-03-08 13:52:03 +0530

    Add files via upload

---
+++ Models/AdminLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DelsProject.Models
+{
+    public class AdminLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Verify { get; set; }
+    }
+}
